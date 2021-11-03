5c104fc87cc6f8b35b48847bf184cdd7b3685fd1
Juliette Alimena <juliette.alimena@cern.ch>
2021-11-03 15:12:27 +0100

    add ALP madgraph model files

---
+++ case-studies/BSM/LLP/ALP_NLO_UFO/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.43
+# Mathematica version: 12.1.1 for Mac OS X x86 (64-bit) (June 19, 2020)
+# Date: Mon 19 Apr 2021 15:23:43
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
